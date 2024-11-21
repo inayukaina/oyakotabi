c6d1398991e3a5eae97428c67a46f28ff3001bee
inayukaina <inayukaina@gmail.com>
2024-11-21 15:26:09 +0900

    tripsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_18_093426) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_21_055556) do
+  create_table "trips", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "budget_total", default: 0
+    t.date "start_date"
+    t.date "end_date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_18_093426) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
