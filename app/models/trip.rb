ccd97b2acdd29d655bc18220a0e51b644b6c20bc
inayukaina <inayukaina@gmail.com>
2024-11-21 21:04:32 +0900

    バリデーションの設定

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
-  has_many :trip_prefectures
+  has_many :trip_prefectures, dependent: :destroy
   has_many :prefectures, through: :trip_prefectures
+
+  validates :budget_total, numericality: { greater_than_or_equal_to: 0 }
+  
+
 end
