f94b105cd5158a9ae888b4119e309c4868953e14
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-21 21:04:32 +0900

    バリデーションの設定

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,4 +1,6 @@
 class Prefecture < ApplicationRecord
   has_many :trip_prefectures
   has_many :trips, through: :trip_prefectures
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/trip_prefecture.rb
+++ app/models/trip_prefecture.rb
@@ -1,4 +1,7 @@
 class TripPrefecture < ApplicationRecord
   belongs_to :trip
   belongs_to :prefecture
+
+  validates :trip_id, presence: true
+  validates :prefecture_id, presence: true
 end
