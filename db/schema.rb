752f417a455f0a7f08d0dfc918a157f3689a9dc1
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-21 15:42:34 +0900

    prefecturesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_21_055556) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_21_063551) do
+  create_table "prefectures", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "region"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "trips", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "budget_total", default: 0
