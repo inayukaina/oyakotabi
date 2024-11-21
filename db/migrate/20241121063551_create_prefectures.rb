7a5e6db47385d64a60f4be7907073b92cc6328e2
inayukaina <inayukaina@gmail.com>
2024-11-21 17:27:23 +0900

    prefecturesテーブルカラムの修正

--- db/migrate/20241121063551_create_prefectures.rb
+++ db/migrate/20241121063551_create_prefectures.rb
@@ -1,8 +1,7 @@
 class CreatePrefectures < ActiveRecord::Migration[7.0]
   def change
     create_table :prefectures do |t|
-      t.string :name, null: false, unique: true
-      t.string :region
+      t.string :name, null: false, index: { unique: true }
       t.timestamps
     end
   end
