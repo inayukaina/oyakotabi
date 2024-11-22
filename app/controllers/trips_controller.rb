54ed06f90d3993965ccff9f28efe35b748d3342c
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-11-22 15:38:06 +0900

    new,createアクションの実装

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,10 +3,21 @@ class TripsController < ApplicationController
   end
 
   def new
-    
+    @trip = Trip.new
   end
 
   def create
-    
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :budget_total, :notes, prefecture_ids: []).merge(user_id: current_user.id)
   end
 end
--- app/models/trip_prefecture.rb
+++ app/models/trip_prefecture.rb
@@ -1,7 +1,4 @@
 class TripPrefecture < ApplicationRecord
   belongs_to :trip
   belongs_to :prefecture
-
-  validates :trip_id, presence: true
-  validates :prefecture_id, presence: true
 end
