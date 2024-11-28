86126bcc2d5c4dad6386f59b04095d251813b692
inayukaina <inayukaina@gmail.com>
2024-11-28 15:37:25 +0900

    フロント修正

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,6 @@
 class TripsController < ApplicationController
   def index
-    @trips = current_user.trips.includes(:prefectures)
+    @trips = current_user.trips.includes(:prefectures).order(start_date: :desc)
     @visited_prefecture_ids = @trips.flat_map { |trip| trip.prefectures.pluck(:id) }.uniq
   end
 
