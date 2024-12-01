82776da4ca32745fd80b6e7f27bbb716479f27bf
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-12-01 23:43:57 +0900

    tripモデル単体テスト

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :trip do
-    
+    association :user
+    budget_total {100000}
+    start_date {Date.today}
+    end_date {Date.today + 3}
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @trip = FactoryBot.build(:trip, user_id: user.id)
+    @prefecture = FactoryBot.create(:prefecture)
+    @trip.prefectures << @prefecture
+  end
+
+  describe '旅行情報の保存' do
+    context '旅行情報を登録できるとき' do
+      it 'start_date、end_date,prefectureを入力すると登録できる' do
+        expect(@trip).to be_valid
+      end
+    end
+
+    context '旅行情報を登録できるとき' do
+      it "start_dateが空では保存できない" do
+        @trip.start_date = nil
+        @trip.valid?
+        expect(@trip.errors.full_messages).to include "Start date can't be blank"
+      end
+      it "end_dateが空では保存できない" do
+        @trip.end_date = nil
+        @trip.valid?
+        expect(@trip.errors.full_messages).to include "End date can't be blank"
+      end
+      it "end_dateがstart_dateより後の場合は登録できない" do
+        @trip.start_date = Date.today
+        @trip.end_date = Date.today - 1
+        @trip.valid?
+        expect(@trip.errors.full_messages).to include "End date は開始日以降の日付を入力してください。"
+      end
+      it "prefectureは最低一つ選択しなければ登録できない" do
+        @trip.prefectures = []
+        @trip.valid?
+        expect(@trip.errors.full_messages).to include "Prefectures は最低1つ選択してください。"
+      end
+      it 'prefectureが重複する場合は保存できない' do
+        @trip.prefectures << @prefecture # すでに作成された `@prefecture` を再追加
+        @trip.valid?
+        expect(@trip.errors.full_messages).to include "Prefectures は重複して選択できません。"
+      end
+    end
+  end
 end
