088617c13880202edbdf279412f5782c5d965f9d
inayukaina <fukutakeyuka@yuMacBook-Air.local>
2024-12-07 20:08:08 +0900

    旅行情報モデル単体テストの日本語化

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -19,28 +19,28 @@ RSpec.describe Trip, type: :model do
       it "start_dateが空では保存できない" do
         @trip.start_date = nil
         @trip.valid?
-        expect(@trip.errors.full_messages).to include "Start date can't be blank"
+        expect(@trip.errors.full_messages).to include "旅行開始日を入力してください"
       end
       it "end_dateが空では保存できない" do
         @trip.end_date = nil
         @trip.valid?
-        expect(@trip.errors.full_messages).to include "End date can't be blank"
+        expect(@trip.errors.full_messages).to include "旅行終了日を入力してください"
       end
       it "end_dateがstart_dateより後の場合は登録できない" do
         @trip.start_date = Date.today
         @trip.end_date = Date.today - 1
         @trip.valid?
-        expect(@trip.errors.full_messages).to include "End date は開始日以降の日付を入力してください。"
+        expect(@trip.errors.full_messages).to include "旅行終了日は旅行開始日以降の日付を入力してください。"
       end
       it "prefectureは最低一つ選択しなければ登録できない" do
         @trip.prefectures = []
         @trip.valid?
-        expect(@trip.errors.full_messages).to include "Prefectures は最低1つ選択してください。"
+        expect(@trip.errors.full_messages).to include "訪問する県は最低1つ選択してください。"
       end
       it 'prefectureが重複する場合は保存できない' do
         @trip.prefectures << @prefecture # すでに作成された `@prefecture` を再追加
         @trip.valid?
-        expect(@trip.errors.full_messages).to include "Prefectures は重複して選択できません。"
+        expect(@trip.errors.full_messages).to include "訪問する県は重複して選択できません。"
       end
     end
   end
