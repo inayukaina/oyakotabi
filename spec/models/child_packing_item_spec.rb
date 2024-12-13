909978070c3600c24592ab0347997a065dc9cf90
inayukaina <inayukaina@gmail.com>
2024-12-13 16:54:02 +0900

    モデル単体テスト日本語化

--- spec/models/child_packing_item_spec.rb
+++ spec/models/child_packing_item_spec.rb
@@ -18,31 +18,31 @@ RSpec.describe ChildPackingItem, type: :model do
       it 'nameが空では保存できない' do
         @child_packing_item.name = nil
         @child_packing_item.valid?
-        expect(@child_packing_item.errors.full_messages).to include("Name can't be blank")
+        expect(@child_packing_item.errors.full_messages).to include("Nameを入力してください")
       end
 
       it 'quantityが空では保存できない' do
         @child_packing_item.quantity = nil
         @child_packing_item.valid?
-        expect(@child_packing_item.errors.full_messages).to include("Quantity is not a number")
+        expect(@child_packing_item.errors.full_messages).to include("Quantityは数値で入力してください")
       end
 
       it 'quantityが0以下では保存できない' do
         @child_packing_item.quantity = 0
         @child_packing_item.valid?
-        expect(@child_packing_item.errors.full_messages).to include("Quantity must be greater than 0")
+        expect(@child_packing_item.errors.full_messages).to include("Quantityは0より大きい値にしてください")
       end
 
       it 'quantityが負の値では保存できない' do
         @child_packing_item.quantity = -1
         @child_packing_item.valid?
-        expect(@child_packing_item.errors.full_messages).to include("Quantity must be greater than 0")
+        expect(@child_packing_item.errors.full_messages).to include("Quantityは0より大きい値にしてください")
       end
 
       it 'tripが関連付けられていない場合は保存できない' do
         @child_packing_item.trip = nil
         @child_packing_item.valid?
-        expect(@child_packing_item.errors.full_messages).to include("Trip must exist")
+        expect(@child_packing_item.errors.full_messages).to include("Tripを入力してください")
       end
     end
   end
