bb42ace034718c71a3edffc8fced331ea2469e32
inayukaina <inayukaina@gmail.com>
2024-12-21 22:17:19 +0900

    完了機能のバリデーション追加

--- app/models/child_packing_item.rb
+++ app/models/child_packing_item.rb
@@ -3,4 +3,5 @@ class ChildPackingItem < ApplicationRecord
 
   validates :name, presence: true
   validates :quantity, numericality: { greater_than: 0 }
+  validates :is_event_completed, inclusion: { in: [true, false] }
 end
