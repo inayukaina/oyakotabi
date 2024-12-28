2baa3ab8da965cb5341818a63f88a901fae00eb5
inayukaina <inayukaina@gmail.com>
2024-12-28 23:21:10 +0900

    エラーメッセージの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,19 +20,19 @@ RSpec.describe User, type: :model do
       it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Eメールを入力してください"
+        expect(@user.errors.full_messages).to include "メールアドレスを入力してください"
       end
       it '重複したemailが存在する場合は登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+        expect(another_user.errors.full_messages).to include('メールアドレスはすでに存在します')
       end
       it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
+        expect(@user.errors.full_messages).to include('メールアドレスは不正な値です')
       end
       it 'passwordが空では登録できない' do
         @user.password = ''
